66a5fb235fabc9ca221d98abed9594524c054e3a
TrinhNgocQuy20235819 <ngocquy353011@gmail.com>
2026-03-04 01:10:15 +0700

    update-helloworld

--- HW01_Trịnh Ngọc Quý_20235819/HelloWorld/HelloWorld.java
+++ HW01_Trịnh Ngọc Quý_20235819/HelloWorld/HelloWorld.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Vui lòng nhập tên của bạn: ");
+        System.out.print("Please enter your name: ");
         String name = scanner.nextLine();
-        System.out.println("Xin chào, " + name + "!");
+        System.out.println("Welcome, " + name + "!");
         scanner.close();
     }
 }
